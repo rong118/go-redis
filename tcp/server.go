e59c6a69a14da14b7e892eca161e907208ad19c2
Rong Li <rongrong.118@hotmail.com>
2023-12-18 20:35:42 -0500

    TCP server can support to close connection by signal

--- tcp/server.go
+++ tcp/server.go
@@ -1,22 +1,19 @@
 package tcp
 
-/**
- * A tcp server
- */
-
 import (
 	"context"
 	"net"
-  "github.com/rong118/go_mini_redis/interface/tcp"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"github.com/rong118/go_mini_redis/interface/tcp"
 )
 
 // Config stores tcp server properties
 type Config struct {
 	Address    string        `yaml:"address"`
 }
-
-// ClientCounter Record the number of clients in the current Godis server
-var ClientCounter int
 
 // ListenAndServeWithSignal binds port and handle requests, blocking until receive stop signal
 func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
@@ -25,25 +22,59 @@ func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
     return err
   }
 
+  // close channel
   closeChan := make(chan struct{})
+  
+  // signal channel, recieve from OS signals
+  sigChan := make(chan os.Signal)
+  signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+
+  // If recieve OS stop signals, sent it to closeChan
+  go func(){
+    sig := <-sigChan 
+    switch sig{
+    case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+      closeChan <- struct{}{}
+    }
+  }()
 
   ListenAndServe(listerner, handler, closeChan)
-
 
   return nil
 }
 
 // ListenAndServe binds port and handle requests, blocking until close
 func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+  
+  // Close listener and handler with recieving closeChan 
+  go func() {
+    <-closeChan
+    _ = listener.Close()
+    _ = handler.Close()
+  }()
+
+  defer func(){
+    _ = listener.Close()
+    _ = handler.Close()
+  }()
   ctx := context.Background()
+
+  var waitDone sync.WaitGroup
   for true {
     conn, err := listener.Accept()
     if err != nil {
       break
     }
+    waitDone.Add(1)
 
     go func (){
+      defer func() {
+        waitDone.Done()
+      }()
+      // Forward request to handler
       handler.Handle(ctx, conn)
     }()
   }
+
+  waitDone.Wait()
 }
