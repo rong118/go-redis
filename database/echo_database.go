7ae52f861753eeef4cc4f83e358ae851a26d0fda
Rong Li <rongrong.118@hotmail.com>
2023-12-30 21:23:13 -0500

    Added database interface and mock database core

---
+++ database/echo_database.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"github.com/rong118/go_mini_redis/interface/resp"
+	"github.com/rong118/go_mini_redis/resp/reply"
+)
+
+type EchoDatabase struct {
+
+}
+
+func NewEchoDataBase() *EchoDatabase {
+  return &EchoDatabase{}
+}
+
+func(e *EchoDatabase) Exec(client resp.Connection, arg []byte) resp.Reply {
+  return reply.MakeBulkReply(arg)
+}
+
+func(e *EchoDatabase) Close() {
+  
+}
+
+func(e *EchoDatabase) AfterClientClose(c resp.Connection) {
+
+}
---
+++ interface/database/database.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"github.com/rong118/go_mini_redis/interface/resp"
+)
+
+type Cmdline = [][]byte
+
+type DataBase interface {
+  Exec(client resp.Connection, arg []byte) resp.Reply
+  Close()
+  AfterClientClose(c resp.Connection)
+}
+
+type DataEntity struct {
+  Data interface{}
+}
