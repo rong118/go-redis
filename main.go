c217c9443ac5e1da14c97cc71863e4b52aff8c84
Rong Li <rongrong.118@hotmail.com>
2024-01-06 12:11:03 -0500

    Updated main to use RESP handler

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/rong118/go_mini_redis/config"
 	"github.com/rong118/go_mini_redis/lib/logger"
+	"github.com/rong118/go_mini_redis/resp/handler"
 	"github.com/rong118/go_mini_redis/tcp"
 )
 
@@ -35,12 +36,19 @@ func main() {
     config.Properties = defaultProperties
   }
 
+  /* This is TCP echo handler example */ 
+  // err := tcp.ListenAndServeWithSignal(
+  //   &tcp.Config{
+  //     Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+  //   }, 
+  //   tcp.MakeHandler())
+
   err := tcp.ListenAndServeWithSignal(
     &tcp.Config{
       Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
     }, 
-    tcp.MakeHandler())
-
+    handler.MakeHandler())
+  
   if err != nil {
     logger.Error(err)
   }
