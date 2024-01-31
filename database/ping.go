ca3386fde46e4a18ced15a8c0aeb3933b4dc3b61
Rong Li <rongrong.118@hotmail.com>
2024-01-31 00:22:06 -0500

    Implemented ping commands set

---
+++ database/ping.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"github.com/rong118/go_mini_redis/interface/resp"
+	"github.com/rong118/go_mini_redis/resp/reply"
+)
+
+func Ping(db *DB, args [][]byte) resp.Reply {
+  return reply.MakePongReply()
+}
+
+// 
+func init() {
+  RegisterCommand("ping", Ping, 1)
+}
